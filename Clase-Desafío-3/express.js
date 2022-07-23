a31ee734ce9d18b96ff82a9af1f58c3c42dd2c9d
Sabri-Rapa <sabrina.rapa@hotmail.com>
2022-07-23 17:47:09 -0300

    Clase 3 - NPM y Express

---
+++ Clase-Desafío-3/express.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+});
+
+//manejo de errores
+server.on("error", error => console.log(`Error en servidor ${error}`))
+
+
+app.get('/', (req, res) => {
+   res.send({ mensaje: 'hola mundo' })
+})
